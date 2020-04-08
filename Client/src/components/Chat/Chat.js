9ed43a45f01551a1d1dbd3116f62e49a06fad129
Austinshepherd <austink.shepherd@gmail.com>
2020-04-08 14:44:24 -0700

    server fix

--- Client/src/components/Chat/Chat.js
+++ Client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'http://penguin.linux.test:5000/';
+  const ENDPOINT = 'https://roomhub-app.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('../server/users');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const router = require('./router');
 
@@ -13,7 +13,6 @@ const io = socketio(server);
 
 app.use(cors());
 app.use(router);
-
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
